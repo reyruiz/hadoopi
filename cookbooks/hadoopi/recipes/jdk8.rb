bcbecc2c6af7bfebda135226001ae112c8056cd4
reyruiz <reynaldo_ruiz@msn.com>
2019-08-27 20:10:18 -0400

    Update jdk8.rb

--- cookbooks/hadoopi/recipes/jdk8.rb
+++ cookbooks/hadoopi/recipes/jdk8.rb
@@ -5,12 +5,12 @@ execute "install dirmng" do
 end
 
 execute "add source"
-  command "echo /"deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main/" | sudo tee /etc/apt/sources.list.d/webupd8team-java.list"
+  command "echo \"deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main\" | sudo tee /etc/apt/sources.list.d/webupd8team-java.list"
   user "root"
 end
 
 execute "add source"
-  command "echo /"deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main/" | sudo tee -a /etc/apt/sources.list.d/webupd8team-java.list" 
+  command "echo \"deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main\" | sudo tee -a /etc/apt/sources.list.d/webupd8team-java.list" 
   user "root" 
 end
 
