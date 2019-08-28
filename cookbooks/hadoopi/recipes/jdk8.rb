708e5de89a74313857d70e3d579dc12d704247c2
reyruiz <reynaldo_ruiz@msn.com>
2019-08-27 20:41:04 -0400

    Update jdk8.rb

--- cookbooks/hadoopi/recipes/jdk8.rb
+++ cookbooks/hadoopi/recipes/jdk8.rb
@@ -1,31 +1,10 @@
-
-execute "install dirmng" do
-	command "apt-get install dirmngr"
-  user "root" 
-end
-
-execute "add source" do
-  command "echo \"deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main\" | sudo tee /etc/apt/sources.list.d/webupd8team-java.list"
-  user "root"
-end
-
-execute "add source" do
-  command "echo \"deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main\" | sudo tee -a /etc/apt/sources.list.d/webupd8team-java.list" 
-  user "root" 
-end
-
-execute "get key" do
-  command "apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886"
-  user "root"
-end
-
 execute "update" do
   command "apt-get update"
   user "root"
 end
 
 excute "Install JDK" do
-  command "apt-get install oracle-java8-jdk"
+  command "sudo apt-get install -y galternatives openjdk-8-jdk"
   user "root"
 end
 
@@ -33,5 +12,3 @@ execute "Check Version" do
  command "java -version"
  user "root"
 end
-
-  
